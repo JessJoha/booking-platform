dcf8949f25869fdd259d4d13d3d5fceec2418b16
JessJoha <jessicajcenteno@gmail.com>
2025-07-06 11:11:34 -0500

    🔧 update deploy users

--- backend/User_Management/findUser/config.py
+++ backend/User_Management/findUser/config.py
@@ -2,9 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
-        f"@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
-    )
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FIND_USER_SERVICE_PORT = int(os.environ.get('FIND_USER_SERVICE_PORT', 5002))
+    LOGIN_SERVICE_PORT = int(os.environ.get('FIND_USER_SERVICE_PORT', 5002))
+    LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info')
+    ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION', 3600))
+    DB_NAME = os.environ.get('DB_NAME')
