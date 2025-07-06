496a7261113cbd51790ceb8be697c542b46da178
JessJoha <jessicajcenteno@gmail.com>
2025-07-05 21:02:05 -0500

    ✅ update reset password test for user microservices

--- backend/User_Management/resetPassword/extensions.py
+++ backend/User_Management/resetPassword/extensions.py
@@ -5,7 +5,7 @@ import os
 db = SQLAlchemy()
 
 redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     decode_responses=True
 )
