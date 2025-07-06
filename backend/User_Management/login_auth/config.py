527ebe084691f15f2893a015d056c84468dc2ca4
JessJoha <jessicajcenteno@gmail.com>
2025-07-06 12:07:59 -0500

    🐛 🔧 update deploy users

--- backend/User_Management/login_auth/config.py
+++ backend/User_Management/login_auth/config.py
@@ -5,7 +5,7 @@ class Config:
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    LOGIN_SERVICE_PORT = int(os.environ.get('REGISTER_SERVICE_PORT', 5000))
+    LOGIN_SERVICE_PORT = int(os.environ.get('LOGIN_SERVICE_PORT', 5000))
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info')
     ACCESS_TOKEN_EXPIRATION = int(os.environ.get('ACCESS_TOKEN_EXPIRATION', 3600))
     DB_NAME = os.environ.get('DB_NAME')
