f2a11afdaf1746fc6379445da0690455cc00a127
JessJoha <jessicajcenteno@gmail.com>
2025-07-06 11:12:59 -0500

    🔧 update deploy users

--- backend/User_Management/findUser/tests/test_find_user.py
+++ backend/User_Management/findUser/tests/test_find_user.py
@@ -3,9 +3,10 @@ import sys
 import os
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+os.environ["TESTING"] = "1"
 
-os.environ['TESTING'] = '1'
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app, db
 from model.findModel import User
