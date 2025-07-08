cccf9f2f30b95d37b01e3357edf45ac4cb46669b
JessJoha <jessicajcenteno@gmail.com>
2025-07-07 21:24:49 -0500

    🐛 Test deploy

--- backend/User_Management/findUser/app.py
+++ backend/User_Management/findUser/app.py
@@ -36,6 +36,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     print(f"Swagger UI available a: http://54.89.82.50:{Config.LOGIN_SERVICE_PORT}/apidocs")
-    print(f"tests")
     print(f"Find User Service running on port {Config.LOGIN_SERVICE_PORT}")
     app.run(debug=True, host='0.0.0.0', port=Config.LOGIN_SERVICE_PORT)
--- backend/User_Management/login_auth/app.py
+++ backend/User_Management/login_auth/app.py
@@ -54,7 +54,6 @@ if __name__ == '__main__':
 
 
     print(f" http://54.89.82.50:{Config.LOGIN_SERVICE_PORT}/apidocs")
-    print(f"tests")
     print(f"{Config.LOGIN_SERVICE_PORT}")
 
     app.run(debug=True, host='0.0.0.0', port=app.config.get('LOGIN_SERVICE_PORT'))
